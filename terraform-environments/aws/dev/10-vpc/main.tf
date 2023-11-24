6a41dfa749afc747395379ad4f6d0fa4e538e92d
Istvan Csoka <istvan@LAPTOP-ST2SC5SP.localdomain>
2023-11-24 18:10:27 +0100

    add ref main git:: and change ref

--- terraform-environments/aws/dev/10-vpc/main.tf
+++ terraform-environments/aws/dev/10-vpc/main.tf
@@ -14,7 +14,7 @@ locals {
 }
 
 module "vpc" {
-  source = "git::https://github.com/csokai/k8sclusters/terraform-modules/aws/vpc?ref=main"
+  source = "git::https://github.com/csokai/k8sclusters/?ref=terraform-modules/aws/vpc"
 
   aws_region       = local.aws_region
   azs              = ["us-east-1a", "us-east-1c", "us-east-1d"]
